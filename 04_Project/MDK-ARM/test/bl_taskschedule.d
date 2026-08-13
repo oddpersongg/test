5306efdab6dc41eb7af334d580a46856e53b6e46
oddpersongg <153888748+oddpersongg@users.noreply.github.com>
2026-08-13 22:17:24 +0800

    Add .gitignore and remove build/debug artifacts from tracking

--- 04_Project/MDK-ARM/test/bl_taskschedule.d
+++
@@ -1,5 +0,0 @@
-test\bl_taskschedule.o: ../BL_Platform/Core/Bl_TaskSchedule.c
-test\bl_taskschedule.o: ../BL_Platform/Core/Bl_TaskSchedule.h
-test\bl_taskschedule.o: ../BL_Platform/Common/Bl_Types.h
-test\bl_taskschedule.o: ../BL_Platform/Config/Bl_TaskSchedule_Cfg.h
-test\bl_taskschedule.o: ../BL_Platform/Config/Bl_TaskSchedule_Lcfg.h
